1d8ca9236325f231229ab548cf3be2acda822244
pumbaa666 <pumbaa666@gmail.com>
2021-01-20 09:35:40 +0100

    wip

--- signal-cli/readMessage.sh
+++ signal-cli/readMessage.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 myPhoneNumber=$(cat ./conf/SENDER_PHONE_NUMBER)
 
-if [ -z "$1" ]; then
-        echo "Enter recipient number"
-        exit -1
-fi
-writeTo=$1
+result=$(signal-cli -u $myPhoneNumber -o json receive)
+echo $result
 
-if [ -z "$2" ]; then
-        echo "Enter message"
-        exit -1
-fi
-message=$2
-message=$message"
----
-Sent from Debian"
 
-# Install signal-cli
-# https://github.com/AsamK/signal-cli/wiki/Quickstart
-signal-cli -u $myPhoneNumber send -m "$message" $writeTo
+#{"envelope":{"source":"+41763773595","sourceDevice":1,"timestamp":1611073933847,"syncMessage":{"sentMessage":{"timestamp":1611073933847,"message":"Lalala","expiresInSeconds":0,"viewOnce":false,"mentions":[],"attachments":[],"destination":"+41795561709"}}}}
+echo "------"
+echo 
+senderNumber=$(echo $result | jq '.envelope.source')
+message=$(echo $result | jq '.envelope.syncMessage.sentMessage.message')
+timestamp=$(echo $result | jq '.envelope.syncMessage.sentMessage.timestamp')
+attachments=$(echo $result | jq '.envelope.syncMessage.sentMessage.attachments
+
+echo "[$senderNumber] $timestamp > $message"
+
